f67b84e7885c420cf13f485486def642d83c5f9b
lucasparco <47321330+lucasparco@users.noreply.github.com>
2019-02-08 12:33:59 -0300

    Add files via upload

---
+++ IngresoUTN2019-master/1-EntradaSalida/jsEntradaSalida-6.js
@@ -0,0 +1,17 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{ var num1 ;
+    
+var num2 ;
+var suma;
+
+num1= document.getElementById("numeroUno").value;
+num2= document.getElementById("numeroDos").value;
+suma= num1+num2;
+alert ("la suma es " + suma);
+
+}
+
+
---
+++ IngresoUTN2019-master/1-EntradaSalida/jsEntradaSalida-9.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar el importe por ID ,
+transformarlo a entero (parseInt), luego
+mostrar el importe con un aumento del 10 %
+en el cuadro de texto "RESULTADO".*/
+function MostrarAumento()
+{
+var sueldo;
+var aumento;
+var resultado;
+sueldo= parseInt(document.getElementById("sueldo").value);
+aumento= sueldo*10/100;
+resultado= sueldo+aumento;
+alert("aumento " + aumento);
+documento.getElementById("resultado").value=nuevosueldo;
+}
