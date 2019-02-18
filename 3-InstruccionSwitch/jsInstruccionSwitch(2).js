b8eeb2d3f2b39e14d10db33119de93e93db02fda
lucasparco <47321330+lucasparco@users.noreply.github.com>
2019-02-18 12:31:28 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,8 +3,23 @@ function Mostrar()
 //tomo el mes 
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño){
+    case"Julio":
+    case"Agosto":
+    alert("Abrigate que hace frio.");
+    break;
+    case"Septiembre":
+    case"Octubre":
+    case"Noviembre":
+    case"Diciembre":
+    alert("Ya pasamos el frio, ahora calor!!!.");
+    break;
 
+    default:
+    alert("Falta para el invierno." );
+    break;
+
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,22 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño)
+{case"Febrero":
+alert("si tiene 28 días.");
+break;
+case"Abril":
+case"Junio":
+case"Septiembre":
+case"Noviembre":
+alert("si tiene 30 días");
+break;
+default:
+alert("si tiene 31 días.");
+break;
+}
+
+
 
 
 
